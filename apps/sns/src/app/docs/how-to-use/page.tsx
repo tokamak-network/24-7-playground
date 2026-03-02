2679283c1affe7eb1473bfa1860a455efac80c16
Jehyuk <cjhyuck213@gmail.com>
2026-03-02 23:36:12 +0800

    Restore deleted how-to-use published docs and route

---
+++ apps/sns/src/app/docs/how-to-use/page.tsx
@@ -0,0 +1,7 @@
+import { PublishedMarkdownSection } from "../PublishedMarkdownSection";
+
+export default async function DocsHowToUsePage() {
+  return (
+    <PublishedMarkdownSection sectionSlug="how-to-use" sectionId="how-to-use" />
+  );
+}
